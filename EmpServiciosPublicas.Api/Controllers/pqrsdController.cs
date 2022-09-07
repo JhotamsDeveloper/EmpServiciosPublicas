771f07a26c68f73a615d9f055172a39d3e4aad9a
JhotaMS <Jhotams.developer@gmail.com>
2022-09-06 23:19:49 -0500

    Clean architecture implementation

--- EmpServiciosPublicas.Api/Controllers/pqrsdController.cs
+++ EmpServiciosPublicas.Api/Controllers/pqrsdController.cs
@@ -20,8 +20,9 @@ namespace EmpServiciosPublicas.Api.Controllers
             _mediator = mediator;
         }
 
-        [HttpGet("{Id}", Name = "GetByType")]
+        //[HttpGet("{Id}", Name = "GetByType")]
         //[Authorize(Roles = "Administrator")]
+        [HttpGet("GetPqrsdByType/{id?}")]
         [ProducesResponseType(typeof(IEnumerable<PqrsdMv>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<IEnumerable<PqrsdMv>>> GetPqrsdByTypePqrsd(string Id)
         {
@@ -29,16 +30,27 @@ namespace EmpServiciosPublicas.Api.Controllers
             return Ok(result);
         }
 
-        [HttpPost(Name = "CreateAnonymousPQRSD")]
+        //[HttpPost(Name = "CreateAnonymousPQRSD")]
         //[Authorize(Roles = "Administrator")]
+        [HttpPost("CreateAnonymous")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         public async Task<ActionResult<string>> CreateAnonymousPQRSD([FromForm] CreateAnonymousCommand command)
         {
             return await _mediator.Send(command);
         }
 
-        [HttpPut(Name = "UpdateAnonymousPQRSD")]
+        //[HttpPost(Name = "CreateAnonymousPQRSD")]
         //[Authorize(Roles = "Administrator")]
+        [HttpPost("Create")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<string>> CreatePQRSD([FromForm] CreateAnonymousCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        //[HttpPut(Name = "UpdateAnonymousPQRSD")]
+        //[Authorize(Roles = "Administrator")]
+        [HttpPut("UpdateAnonymous")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesDefaultResponseType]
@@ -48,8 +60,9 @@ namespace EmpServiciosPublicas.Api.Controllers
             return NoContent();
         }
 
-        [HttpDelete("{id}", Name = "DeleteAnonymousPQRSD")]
+        //[HttpDelete("{id}", Name = "DeleteAnonymousPQRSD")]
         //[Authorize(Roles = "Administrator")]
+        [HttpDelete("Delete/{id}")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         [ProducesDefaultResponseType]
